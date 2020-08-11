87be6a309ef0999cfc21f8b3b399eb71d86e6bf8
Marcelo Bueno <marcelo.bueno@planejamento.gov.br>
2020-08-11 11:08:39 -0300

    Alteraçao do server.ts para permitir a utilizaçao de CORS tanto em produçao quanto local.

--- src/server.ts
+++ src/server.ts
@@ -7,11 +7,32 @@ const app = express();
 
 const port = process.env.PORT || 3333;
 
-// app.use(cors);
+const allowedOrigins = [
+  `http://localhost:${port}`,
+  'https://bueno-proffy-server.herokuapp.com'
+];
+
+app.use(cors({
+  origin: function(origin, callback){
+    // Allow requests with no origin like mobile apps or curl requests
+    if(!origin) {
+      return callback(null, true);
+    }
+
+    if(allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+
+      return callback(new Error(msg), false);
+    }
+
+    return callback(null, true);
+  }
+}));
+
 app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
-  console.log(`NodeJS is running in ${port}`);
+  console.log(`NodeJS is running in port: ${port};`);
 });
