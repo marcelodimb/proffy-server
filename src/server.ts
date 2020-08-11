11c7c17843f383c58a62593995141baacad6d966
Marcelo Bueno <marcelo.bueno@planejamento.gov.br>
2020-08-11 01:38:11 -0300

    Inserçao da funcionalidade de CORS.

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 import routes from './routes';
 
 const app = express();
 
+const port = process.env.PORT || 3333;
+
+// app.use(cors);
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(port, () => {
+  // eslint-disable-next-line no-console
+  console.log(`NodeJS is running in ${port}`);
+});
